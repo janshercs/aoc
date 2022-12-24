c1e3ecb2e78be90bbff2af999e43c7eea6d1126e
jansen her <jansen.cs@gmail.com>
2022-12-24 23:16:31 +0800

    Day 13. Half baked solution.

--- solutions/solutions/day_thirteen/day_thirteen.go
+++ solutions/solutions/day_thirteen/day_thirteen.go
@@ -3,17 +3,67 @@ package daythirteen
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"io"
+	"os"
+	"reflect"
 	utils "solutions/aoc_utils"
+	"sort"
 )
 
-type pair struct {
+type packetPair struct {
 	lhs, rhs []interface{}
 }
 
-func parseInput(r io.Reader) (p []pair) {
+func (p packetPair) inOrder() (bool, bool) {
+	shorterLen := shorter(len(p.lhs), len(p.rhs))
+
+	for i, j := 0, 0; i < shorterLen; i, j = i+1, j+1 {
+		// find out type of lhs and rhs
+		// decide on a way to compare both
+		// if int convert to slice and compare slice v.s. slice
+
+		// find out if either are lists first.
+		if oneIsList(p.lhs[i], p.rhs[j]) {
+			subPacket := packetPair{
+				lhs: convertToList(p.lhs[i]),
+				rhs: convertToList(p.rhs[i]),
+			}
+
+			order, cont := subPacket.inOrder()
+
+			if !order {
+				return false, false
+			}
+
+			if !cont {
+				return true, false
+			}
+
+		} else {
+			if p.lhs[i].(float64) < p.rhs[j].(float64) {
+				return true, false
+			}
+			if p.lhs[i].(float64) > p.rhs[j].(float64) {
+				return false, false
+			}
+		}
+	}
+
+	if len(p.lhs) < len(p.rhs) {
+		return true, false
+	}
+
+	if len(p.lhs) > len(p.rhs) {
+		return false, false
+	}
+
+	return true, true
+}
+
+func parseInput(r io.Reader) (p []packetPair) {
 	scanner := bufio.NewScanner(r)
-	currentPair := pair{}
+	currentPair := packetPair{}
 	for scanner.Scan() {
 		if scanner.Text() == utils.EmptyString {
 			p = append(p, currentPair)
@@ -36,4 +86,115 @@ func parseLine(s string) []interface{} {
 	out := []interface{}{}
 	json.Unmarshal([]byte(s), &out)
 	return out
+}
+
+func shorter(a, b int) int {
+	if b < a {
+		return b
+	}
+	return a
+}
+
+// returns true if both are slices
+func bothAreList(a, b interface{}) bool {
+	return reflect.TypeOf(a).Kind() == reflect.Slice && reflect.TypeOf(b).Kind() == reflect.Slice
+}
+
+func oneIsList(a, b interface{}) bool {
+	return reflect.TypeOf(a).Kind() == reflect.Slice || reflect.TypeOf(b).Kind() == reflect.Slice
+}
+
+func convertToList(a interface{}) []interface{} {
+	if reflect.TypeOf(a).Kind() == reflect.Slice {
+		return a.([]interface{})
+	}
+	// if not slice, it's integer
+	return []interface{}{a}
+}
+
+func truncateToOneMemberList(a interface{}) []interface{} {
+	if reflect.TypeOf(a).Kind() == reflect.Slice {
+		aSlice := a.([]interface{})
+		if len(aSlice) > 0 {
+			return []interface{}{aSlice[0]} // returns only 1 member
+		}
+		return aSlice // returns empty slice
+	}
+	// if not slice, it's integer
+	return []interface{}{a}
+}
+
+func solutionA() int {
+	f, err := os.Open("./day_thirteen.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	packetPairs := parseInput(f)
+
+	indexes := []int{}
+	for i := range packetPairs {
+		if b, _ := packetPairs[i].inOrder(); b {
+			indexes = append(indexes, i)
+		}
+	}
+
+	fmt.Printf("indexes that are correct: %v", indexes)
+
+	sum := len(indexes) // to take care of the 1 based system they want us to sum in.
+	for _, i := range indexes {
+		sum += i
+	}
+
+	return sum
+}
+
+// sort all the packets using the above as a sorting algorithm
+type packetArray [][]interface{}
+
+func (a packetArray) Len() int      { return len(a) }
+func (a packetArray) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a packetArray) Less(i, j int) bool {
+	p := packetPair{a[i], a[j]}
+	b, _ := p.inOrder()
+	return b
+}
+
+func parseAllPackets(r io.Reader) (p packetArray) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		if scanner.Text() == utils.EmptyString {
+			continue
+		}
+		p = append(p, parseLine(scanner.Text()))
+	}
+	return p
+}
+
+func solutionB() int {
+	f, err := os.Open("./day_thirteen.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	pArray := parseAllPackets(f)
+	pArray = append(pArray, parseLine("[[6]]"))
+	pArray = append(pArray, parseLine("[[2]]"))
+	sort.Sort(pArray)
+
+	decoderIndex := []int{}
+	for _, p := range pArray {
+		fmt.Println(p)
+		// need to figure out how to compare interface!!
+
+		// if len(p) == 1 {
+		// 	if p[0] == parseLine("[[6]]")[0] || p[0] == parseLine("[[2]]")[0] {
+		// 		decoderIndex = append(decoderIndex, i+1)
+		// 	}
+		// }
+	}
+	fmt.Println(decoderIndex)
+	return 0
 }
--- solutions/solutions/day_thirteen/day_thirteen_test.go
+++ solutions/solutions/day_thirteen/day_thirteen_test.go
@@ -15,7 +15,7 @@ func TestParseInput(t *testing.T) {
 [4]
 `
 
-	assert.Equal(t, []pair{
+	assert.Equal(t, []packetPair{
 		{[]interface{}{float64(1)}, []interface{}{float64(2)}},
 		{[]interface{}{float64(3)}, []interface{}{float64(4)}},
 	}, parseInput(strings.NewReader(in)))
@@ -26,4 +26,110 @@ func TestParseLine(t *testing.T) {
 	assert.Equal(t, []interface{}{}, parseLine("[]"))
 	assert.Equal(t, []interface{}{float64(1)}, parseLine("[1]"))
 	assert.Equal(t, []interface{}{float64(1), float64(32), []interface{}{float64(3)}}, parseLine("[1, 32, [3]]"))
+}
+
+// purely developed this based on TDD: I had no idea or inspiration on how to solve this...
+func TestInOrder(t *testing.T) {
+	testCases := []struct {
+		desc string
+		pair packetPair
+		want bool
+	}{
+		{
+			pair: packetPair{
+				lhs: parseLine("[1,1,3,2,2,2,2]"),
+				rhs: parseLine("[1,1,5,1,1]"),
+			},
+			want: true,
+		},
+		{
+			pair: packetPair{
+				lhs: parseLine("[[1],[2,3,4]]"),
+				rhs: parseLine("[[1],4]"),
+			},
+			want: true,
+		},
+		{
+			desc: "list in before should pass, and after that should fail.",
+			pair: packetPair{
+				lhs: parseLine("[[],5]"),
+				rhs: parseLine("[[1],4]"),
+			},
+			want: true,
+		},
+		{
+			desc: "lists in before with same length should pass, and after that should fail.",
+			pair: packetPair{
+				lhs: parseLine("[[2],5]"),
+				rhs: parseLine("[[3],4]"),
+			},
+			want: true,
+		},
+		{
+			pair: packetPair{
+				lhs: parseLine("[7,7,7,7]"),
+				rhs: parseLine("[7,7,7]"),
+			},
+			want: false,
+		},
+		{
+			desc: "test comparing nested lists",
+			pair: packetPair{
+				lhs: parseLine("[[4,4],4,4]"),
+				rhs: parseLine("[[4,4],4,4,4]"),
+			},
+			want: true,
+		},
+		{
+			desc: "test comparing RHS nested list contains smaller element",
+			pair: packetPair{
+				lhs: parseLine("[[4,4],4,4]"),
+				rhs: parseLine("[[4,3],4,4,4]"),
+			},
+			want: false,
+		},
+		{
+			desc: "test comparing nested list against int",
+			pair: packetPair{
+				lhs: parseLine("[9]"),
+				rhs: parseLine("[[8,7,6]]"),
+			},
+			want: false,
+		},
+		{
+			desc: "test different lengths of empty nested lists",
+			pair: packetPair{
+				lhs: parseLine("[[[]]]"),
+				rhs: parseLine("[[]]"),
+			},
+			want: false,
+		},
+		{
+			desc: "test complex input",
+			pair: packetPair{
+				lhs: parseLine("[1,[2,[3,[4,[5,6,7]]]],8,9]"),
+				rhs: parseLine("[1,[2,[3,[4,[5,6,0]]]],8,9]"),
+			},
+			want: false,
+		},
+		{
+			desc: "test complex input",
+			pair: packetPair{
+				lhs: parseLine("[]"),
+				rhs: parseLine("[3]"),
+			},
+			want: true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			b, _ := tc.pair.inOrder()
+			assert.Equal(t, tc.want, b)
+		})
+	}
+}
+
+func TestSolution(t *testing.T) {
+	// assert.Equal(t, 5503, solutionA())
+	assert.Equal(t, 0, solutionB())
 }
